1801ec6a0ed82262515d59c9c8ae5e0f16863221
oiov <hi@oiov.dev>
2025-07-18 10:48:31 +0800

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -60,7 +60,7 @@ export default async function RootLayout({
   return (
     <html lang='zh-CN' suppressHydrationWarning>
       <head>
-        <link rel="preload" href="https://uv.do/s/sextv" as="fetch">
+        <link rel="preload" href="https://uv.do/s/sextv" as="fetch" />
         {/* 将配置序列化后直接写入脚本，浏览器端可通过 window.RUNTIME_CONFIG 获取 */}
         {/* eslint-disable-next-line @next/next/no-sync-scripts */}
         <script
